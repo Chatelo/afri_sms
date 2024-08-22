cb670b094949f28d45ac3191a406996377a72533
Benard Ronoh <biikate48@gmail.com>
2024-08-22 15:13:32 +0300

    flask migrate

--- config.py
+++ config.py
@@ -1,10 +1,19 @@
+import os
+
+
 # Secret key for generating tokens
 SECRET_KEY='xu4oGFjn1_6PkiydrZitCE9JIozEZv3jMrvHl0T66Pk'
 SECURITY_PASSWORD_SALT='250411626211731261460976096525945109056'
 
+database_url = os.environ.get('DATABASE_URL', 'postgres://default:7ZYwLXap4xlP@ep-raspy-lake-a4ot0dgi-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require')
+if database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+SQLALCHEMY_DATABASE_URI = database_url
+
 # Database configuration
 # SQLALCHEMY_DATABASE_URI='postgresql://postgres@localhost:5432/sms'
-SQLALCHEMY_DATABASE_URI='postgres://default:7ZYwLXap4xlP@ep-raspy-lake-a4ot0dgi-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require'
+# SQLALCHEMY_DATABASE_URI='postgres://default:7ZYwLXap4xlP@ep-raspy-lake-a4ot0dgi-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require'
 # SQLALCHEMY_DATABASE_URI='postgresql://postgres:QsdQEVlCuBeuBcCmiHzXOTkkAESSNGzi@postgres.railway.internal:5432/railway'
 SQLALCHEMY_TRACK_MODIFICATIONS=False
 SQLALCHEMY_ENGINE_OPTIONS={"pool_pre_ping": True,}
